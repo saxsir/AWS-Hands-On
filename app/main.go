48a8ec1729bf4c083fd7622df4358d46e5feaf3a
saxsir <saxsir.256+github@gmail.com>
2018-11-27 20:22:46 +0900

    prepare index.html

--- app/main.go
+++ app/main.go
@@ -16,35 +16,32 @@ func main() {
 	if dataSourceName == "" {
 		dataSourceName = "root:password@tcp(127.0.0.1:13306)/sampledb"
 	}
+	db, err := sql.Open("mysql", dataSourceName)
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
 
-	handler := func(w http.ResponseWriter, r *http.Request) {
-		db, err := sql.Open("mysql", dataSourceName)
-		if err != nil {
-			panic(err.Error())
-		}
-		defer db.Close()
+	// health check
+	http.HandleFunc("/ok", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(200)
+	})
 
+	http.Handle("/", http.FileServer(http.Dir("static")))
+	http.HandleFunc("/event", func(w http.ResponseWriter, r *http.Request) {
 		stmt, err := db.Prepare("INSERT INTO eventlog(at, name, value) values(NOW(), ?, ?)")
 		if err != nil {
-			panic(err.Error())
+			panic(err)
 		}
 		defer stmt.Close()
 
 		name := r.URL.Query().Get("name")
 		value := r.URL.Query().Get("value")
-
 		_, _ = stmt.Exec(name, value)
-	}
 
-	http.HandleFunc("/ok", func(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(200)
+		w.Write([]byte("success"))
 	})
-	http.HandleFunc("/ping", func(w http.ResponseWriter, r *http.Request) {
-		w.WriteHeader(200)
-		w.Write([]byte("pong"))
-	})
-
-	http.HandleFunc("/event", handler)
 
 	// start server
 	if err := http.ListenAndServe(":8081", nil); err != nil {
